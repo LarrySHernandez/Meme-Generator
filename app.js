86e5d54df646f7169ad30a90ed1276523b6be130
larryh2612 <larryh2612@gmail.com>
2024-05-31 00:06:27 -0400

    sizing function addition

--- app.js
+++ app.js
@@ -2,6 +2,26 @@ let urlInput = document.getElementById('urlInput');
 let imageSubmitBtn = document.getElementById('imageSubmitBtn');
 let imageChooserBox = document.getElementsByClassName('imageChooserBox');
 let memeImageBorder = document.getElementById('memeImageBorder');
+let heightInput = document.getElementById("heightNumber");
+let widthInput = document.getElementById("widthNumber");
+let sizingInputs = document.getElementById("numbers");
+
+
+
+//Default Settings
+heightInput.value = '300';
+widthInput.value = '300';
+
+
+heightInput.addEventListener('input', function (event) {
+    memeImageBorder.style.height = heightInput.value + 'px';
+    memeImageBorder.style.bottom = (((635 - heightInput.value) * .5373134328358209) + 140) + "px";
+})
+
+widthInput.addEventListener('input', function (event) {
+    memeImageBorder.style.width = widthInput.value + 'px';
+    memeImageBorder.style.left = (((860 - widthInput.value) * .5178571428571429) + -10) + "px";
+})
 
 
 imageSubmitBtn.addEventListener('click', function (e) {
@@ -9,7 +29,5 @@ imageSubmitBtn.addEventListener('click', function (e) {
     let url = urlInput.value
     memeImageBorder.style.zIndex = '2';
     memeImageBorder.style.backgroundImage = `url("${url}")`
-
-    console.log(url.length);
 });
 
