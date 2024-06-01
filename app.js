80de03d8077bee5004fe7e5a6c7f245ba074a5ad
larryh2612 <larryh2612@gmail.com>
2024-06-01 00:00:42 -0400

    sizing updates

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ let memeImageBorder = document.getElementById('memeImageBorder');
 let heightInput = document.getElementById("heightNumber");
 let widthInput = document.getElementById("widthNumber");
 let sizingInputs = document.getElementById("numbers");
-
+let addBtn = document.getElementById('addBtn');
+let memeText = document.getElementById('memeText');
 
 
 //Default Settings
@@ -13,14 +14,47 @@ heightInput.value = '300';
 widthInput.value = '300';
 
 
+function createText() {
+    let text = memeText.value;
+    let textBox = document.createElement('p');
+    textBox.innerText = text;
+    textBox.style.height = '25px';
+    textBox.style.width = '70px';
+    textBox.style.position = 'absolute';
+    console.dir(textBox);
+    memeImageBorder.appendChild(textBox);
+
+}
+
+
+
+addBtn.addEventListener('click', function () {
+    if (memeImageBorder.style.zIndex.length > 1) {
+        createText();
+
+    }
+});
+
 heightInput.addEventListener('input', function (event) {
-    memeImageBorder.style.height = heightInput.value + 'px';
-    memeImageBorder.style.bottom = (((635 - heightInput.value) * .5373134328358209) + 140) + "px";
+    if (heightInput.value <= 635 && heightInput.value >= 300) {
+        memeImageBorder.style.height = heightInput.value + 'px';
+        memeImageBorder.style.bottom = (((635 - heightInput.value) * .5373134328358209) + 140) + "px";
+    } else if (heightInput > 635) {
+        heightInput.value = 635;
+    } else if (heightInput < 300) {
+        heightInput.value = 300;
+    }
 })
 
 widthInput.addEventListener('input', function (event) {
-    memeImageBorder.style.width = widthInput.value + 'px';
-    memeImageBorder.style.left = (((860 - widthInput.value) * .5178571428571429) + -10) + "px";
+    if (widthInput.value <= 860 && widthInput.value >= 300) {
+        memeImageBorder.style.width = widthInput.value + 'px';
+        memeImageBorder.style.left = (((860 - widthInput.value) * .5178571428571429) + -10) + "px";
+    } else if (widthInput > 860) {
+        widthInput.value = 860;
+    } else if (widthInput < 300) {
+        widthInput.value = 300;
+    }
 })
 
 
